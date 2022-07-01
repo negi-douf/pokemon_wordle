9dae44e578a46ff8b9dfd455bbd6a246bc8b1b6c
USER <user@USERnoMacBook-Pro.local>
2022-07-02 07:07:02 +0900

    ラベルテストを追加した

---
+++ tests/test_game.py
@@ -0,0 +1,32 @@
+import pytest
+from pokemon_wordle import Color, Game, Pokemon
+
+
+class TestGame:
+
+    @pytest.mark.parametrize('responce,expected',[
+        ('フシギダネ',[Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN]),
+        ('フ・ギ・ネ',[Color.GREEN,Color.NONE,Color.GREEN,Color.NONE,Color.GREEN]),
+        ('ネフシギダ',[Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW]),
+        ('ネフシギギ',[Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.NONE]),
+        ('フフフネギ',[Color.GREEN,Color.NONE,Color.NONE,Color.YELLOW,Color.YELLOW]),
+    ])
+    def test_label_when_same_char_not_in_pokemon_name(self,responce: str,expected: list[Color]):
+        choiced_pokemon = Pokemon(name='フシギダネ',type_01='くさ',type_02='どく')
+        game = Game(answer_pokemon=choiced_pokemon)
+        actual = game.label(responce)
+        assert actual == expected
+
+    @pytest.mark.parametrize('responce,expected',[
+        ('ゴーリキー',[Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN,Color.GREEN]),
+        ('ゴ・リ・ー',[Color.GREEN,Color.NONE,Color.GREEN,Color.NONE,Color.GREEN]),
+        ('ーゴーリキ',[Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW]),
+        ('ーゴーリリ',[Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.YELLOW,Color.NONE]),
+        ('ゴゴゴゴゴ',[Color.GREEN,Color.NONE,Color.NONE,Color.NONE,Color.NONE]),
+        ('ゴーゴーリ',[Color.GREEN,Color.GREEN,Color.NONE,Color.YELLOW,Color.YELLOW]),
+    ])
+    def test_label_when_same_char_in_pokemon_name(self,responce: str,expected: list[Color]):
+        choiced_pokemon = Pokemon(name='ゴーリキー',type_01='かくとう')
+        game = Game(answer_pokemon=choiced_pokemon)
+        actual = game.label(responce)
+        assert actual == expected
