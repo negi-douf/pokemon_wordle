daa307117b8fbcc2e2df1346bd0dd668ebf07321
USER <user@USERnoMacBook-Pro.local>
2022-07-03 14:12:54 +0900

    restructureに伴いTestも追従させた

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,32 +1,30 @@
 import pytest
-from pokemon_wordle import Color, Game, Pokemon
+from pokemon_wordle import Color, ColorLabels
 
 
-class TestGame:
+class TestColorLabels:
 
-    @pytest.mark.parametrize('responce,expected', [
+    @pytest.mark.parametrize('response,expected', [
         ('フシギダネ', [Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN]),
-        ('フ・ギ・ネ', [Color.GREEN, Color.NONE, Color.GREEN, Color.NONE, Color.GREEN]),
+        ('フ・ギ・ネ', [Color.GREEN, Color.WHITE, Color.GREEN, Color.WHITE, Color.GREEN]),
         ('ネフシギダ', [Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW]),
-        ('ネフシギギ', [Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.NONE]),
-        ('フフフネギ', [Color.GREEN, Color.NONE, Color.NONE, Color.YELLOW, Color.YELLOW]),
+        ('ネフシギギ', [Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.WHITE]),
+        ('フフフネギ', [Color.GREEN, Color.WHITE, Color.WHITE, Color.YELLOW, Color.YELLOW]),
     ])
-    def test_label_when_same_char_not_in_pokemon_name(self, responce: str, expected: list[Color]):
-        choiced_pokemon = Pokemon(name='フシギダネ', type_01='くさ', type_02='どく')
-        game = Game(answer_pokemon=choiced_pokemon)
-        actual = game.label(responce)
+    def test_colorlabel_when_same_char_not_in_pokemon_name(self, response: str, expected: list[Color]):
+        color_labels = ColorLabels(answer='フシギダネ', response=response)
+        actual = color_labels.labels
         assert actual == expected
 
-    @pytest.mark.parametrize('responce,expected', [
+    @pytest.mark.parametrize('response,expected', [
         ('ゴーリキー', [Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN]),
-        ('ゴ・リ・ー', [Color.GREEN, Color.NONE, Color.GREEN, Color.NONE, Color.GREEN]),
+        ('ゴ・リ・ー', [Color.GREEN, Color.WHITE, Color.GREEN, Color.WHITE, Color.GREEN]),
         ('ーゴーリキ', [Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW]),
-        ('ーゴーリリ', [Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.NONE]),
-        ('ゴゴゴゴゴ', [Color.GREEN, Color.NONE, Color.NONE, Color.NONE, Color.NONE]),
-        ('ゴーゴーリ', [Color.GREEN, Color.GREEN, Color.NONE, Color.YELLOW, Color.YELLOW]),
+        ('ーゴーリリ', [Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.WHITE]),
+        ('ゴゴゴゴゴ', [Color.GREEN, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE]),
+        ('ゴーゴーリ', [Color.GREEN, Color.GREEN, Color.WHITE, Color.YELLOW, Color.YELLOW]),
     ])
-    def test_label_when_same_char_in_pokemon_name(self, responce: str, expected: list[Color]):
-        choiced_pokemon = Pokemon(name='ゴーリキー', type_01='かくとう')
-        game = Game(answer_pokemon=choiced_pokemon)
-        actual = game.label(responce)
+    def test_label_when_same_char_in_pokemon_name(self, response: str, expected: list[Color]):
+        color_labels = ColorLabels(answer='ゴーリキー', response=response)
+        actual = color_labels.labels
         assert actual == expected
